6a2c9b3f4aeb402882b95dedb46ff58a85ece10e
Yingjie <1275963@gmail.com>
2016-02-23 12:20:08 +0800

    It is not a good practice about ListNode

--- swap_nodes_in_pairs/Solution.java
+++ swap_nodes_in_pairs/Solution.java
@@ -1,8 +1,15 @@
 package swap_nodes_in_pairs;
 
-import ListNode.ListNode;
-
+// https://leetcode.com/problems/swap-nodes-in-pairs/
 public class Solution {
+    public ListNode swapParis(ListNode head) {
+        ListNode dummyhead;
+        dummyhead = head; // single head
+        while(head.next != null) {
+            
+        }
+        
+    }
     
     public static void main(String[] args) {
         ListNode test = new ListNode(5);
