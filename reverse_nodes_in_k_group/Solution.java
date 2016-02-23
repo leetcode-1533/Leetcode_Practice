b6e9b1859f0c5279b03279d2efb3d82c8acc3d98
Yingjie <1275963@gmail.com>
2016-02-23 15:47:17 +0800

    Part 1, with one tail, working on 2 and more

--- reverse_nodes_in_k_group/Solution.java
+++ reverse_nodes_in_k_group/Solution.java
@@ -6,25 +6,31 @@ import edu.princeton.cs.algs4.StdOut;
 public class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         int i = 0;
-        ListNode dummy = new ListNode(0);
-        ListNode pnew = dummy;
-        
+        ListNode rec = new ListNode(0);
+        ListNode p = head;
         ListNode pold = head;
+        ListNode temp;                
         
-        // test for k
-        while(i++ < (k - 1) && head.next != null) // head in the last section's tail, guaranteed section complete
-            head = head.next;
-        
-        if(i == k)
-            StdOut.println("Success");
-        head.next = null; // cut the section
-        
-        while(pold != null) {
-            pnew.next = pold;
-            pold = pold.next;
+        while(head != null) {
+            pold = head;
+
+            // test for k
+            while(i++ < (k - 1) && head.next != null) // head in the last section's tail, guaranteed section complete
+                head = head.next;
+            
+            if(i == k) {
+                temp = head.next;
+                head.next = null; // cut the section
+                p = reversetillnull(pold);
+                pold.next = temp;
+                head = temp;
+                i = 0;
+            } else {
+                rec.next = p;
+            }                     
         }
-        
-        return dummy.next;
+        return rec.next;
+            
     }
     public ListNode reversetillnull(ListNode head) {
         ListNode current = head;
@@ -45,8 +51,8 @@ public class Solution {
         ListNode.printlist(test);
 
         Solution sol = new Solution();
-        test = sol.reversetillnull(test);
-        StdOut.println("Rervertillnull");
+        StdOut.println("reverseKGroup");
+        test = sol.reverseKGroup(test, 3);
         ListNode.printlist(test);
 
     }
