f820ff8b2988a85fcfc8f8937ca89ac13c427a93
Yingjie <1275963@gmail.com>
2016-02-29 23:16:58 +0800

    Started

---
+++ insertion_sort_list/Solution.java
@@ -0,0 +1,23 @@
+package insertion_sort_list;
+import delete_node_in_a_linked_list.ListNode;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Solution {
+    
+    public ListNode insertionSortList(ListNode head) {
+        
+    }
+    
+    private void insert(ListNode root, ListNode tar) {
+        
+    }
+
+    
+    public static void main(String[] args) {
+        ListNode test = ListNode.randomeNode(10, 100);
+        StdOut.println(test);
+
+        Solution sol = new Solution();
+    }
+
+}
