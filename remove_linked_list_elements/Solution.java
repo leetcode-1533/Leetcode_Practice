f5bd9549c111a746277f536164c45968dfbcc325
Yingjie <1275963@gmail.com>
2016-02-29 21:45:59 +0800

    A second version

--- remove_linked_list_elements/Solution.java
+++ remove_linked_list_elements/Solution.java
@@ -12,30 +12,28 @@ public class Solution {
     public ListNode removeElements(ListNode head, int val) {
         while(head !=null && head.val == val) 
             head = head.next;
-        
-        ListNode root = head;
-        
-        while(  root != null) {
-        if(root.next != null && root.next.val == val) {
-            root.next = root.next.next;                  
-        } else {
-            root = root.next;
-        }
-        }
+        iterdel(head, val);
         return head;       
     }
 //    ListNode temp = node.next;
 //    node.val = temp.val;
 //    node.next = temp.next;    
     public void iterdel(ListNode root, int val) {
-
+        if(root == null)
+            return;
+        while(root.next != null && root.next.val == val) {
+            root.next = root.next.next;                  
+        } 
+        root = root.next;
+        
+        iterdel(root, val);                  
     }
     
     public static void main(String[] args) {
         ListNode test = ListNode.randomeNode(10, 4 );
         StdOut.println(test);
         Solution sol = new Solution();
-        test = sol.removeElements(test, 1);
+        test = sol.removeElements(test, 3);
         StdOut.println(test);
 
     }
