162f317b4577f3afaa5c5b7d313a024d35894cc3
Yingjie <1275963@gmail.com>
2016-03-03 12:04:57 +0800

    Archive

--- insertion_sort_list/Solution.java
+++ insertion_sort_list/Solution.java
@@ -8,37 +8,9 @@ public class Solution {
         if(head == null) {
             return null;
         }
-        
-        if(head.next != null && head.val > head.next.val) {
-            ListNode temp = head.next;
-            head.next = temp.next;
-            temp.next = head; 
-            head = temp;
-        }
-        ListNode tail = head;
-      
-        ListNode temp = tail;
-        ListNode iter = temp.next;
-        
-        while(iter.next != null) {
-            if(iter.next.val < iter.val) {
-                temp = exch(temp, iter, iter.next);            
-            }
-        }
 
-        
-        return tail;    
     }
-    
-    private ListNode exch(ListNode before, ListNode middle, ListNode after) {
-        ListNode fourth = after.next;
         
-        before.next = after;
-        after.next = middle;
-        middle.next = fourth;
-        
-        return after;  
-    }
 
     
     public static void main(String[] args) {
