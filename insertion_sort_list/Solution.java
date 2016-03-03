8deed3607b70a22996260668c559dbfda6cbe90e
Yingjie <1275963@gmail.com>
2016-03-03 12:15:26 +0800

    Don't hurry, do it Step-by-Step and with pleasure
    37 -> 27 -> 27 -> 20 -> 12
    
    i: 37 -> 27 -> 27 -> 20 -> 12
    
    i: 27 -> 27 -> 20 -> 12
    EXCH: j: 37 -> 27 -> 27 -> 20 -> 12
    
    i: 27 -> 20 -> 12
    EXCH: j: 37 -> 27 -> 27 -> 20 -> 12
    
    i: 20 -> 12
    EXCH: j: 37 -> 27 -> 27 -> 20 -> 12
    EXCH: j: 27 -> 27 -> 20 -> 12
    EXCH: j: 27 -> 20 -> 12
    
    i: 12
    EXCH: j: 37 -> 27 -> 27 -> 20 -> 12
    EXCH: j: 27 -> 27 -> 20 -> 12
    EXCH: j: 27 -> 20 -> 12
    EXCH: j: 20 -> 12

--- insertion_sort_list/Solution.java
+++ insertion_sort_list/Solution.java
@@ -8,18 +8,32 @@ public class Solution {
         if(head == null) {
             return null;
         }
-
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pointer = head;
+        
+        for(ListNode i = head; i != null; i = i.next) {
+            StdOut.println();
+            StdOut.println("i: " + i);
+            for(ListNode j = dummy; j != i; j = j.next) {
+                if(j.next.val > i.val) {
+                    StdOut.println("EXCH: j: " + j.next);
+                }
+            }
+        }
+            
+        
+        return dummy.next;
     }
         
 
     
     public static void main(String[] args) {
-        ListNode test = ListNode.randomeNode(10, 100);
+        ListNode test = ListNode.randomeNode(5, 100);
         StdOut.println(test);
 
         Solution sol = new Solution();
-        test = sol.insertionSortList(test);
-        StdOut.println(test);
+        sol.insertionSortList(test);
     }
 
 }
