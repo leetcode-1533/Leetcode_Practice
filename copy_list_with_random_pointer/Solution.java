662c4789faa369b1e6df7be87ebcb492e62914ee
Yingjie <1275963@gmail.com>
2016-03-05 12:27:38 +0800

    Lable is not index

--- copy_list_with_random_pointer/Solution.java
+++ copy_list_with_random_pointer/Solution.java
@@ -1,4 +1,6 @@
 package copy_list_with_random_pointer;
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Definition for singly-linked list with a random pointer.
@@ -10,6 +12,34 @@ package copy_list_with_random_pointer;
  */
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
-        
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode p = dummy;
+        ArrayList<Integer> litable = new ArrayList<Integer>();
+        ArrayList<RandomListNode> objtable = new ArrayList<RandomListNode>();
+        RandomListNode htemp = head;
+        while(head != null) {
+            p.next = new RandomListNode(head.label);           
+            litable.add(head.random.label);
+            objtable.add(p.next);
+            
+            head = head.next;
+            p = p.next;           
+        }
+        p = dummy.next;
+        while(p != null) {
+            int index = litable.indexOf(htemp.random.label);
+//            StdOut.printf("Label: %d, Index: %d\n", htemp.random.label, index);
+            p.random = objtable.get(htemp.random.label);
+            p = p.next;
+            htemp = htemp.next;
+        }     
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        RandomListNode test = RandomListNode.testcase();
+        StdOut.println(test);
+        Solution sol = new Solution();
+        RandomListNode dclone = sol.copyRandomList(test);
+        StdOut.println(dclone);
     }
 }
