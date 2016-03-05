4d744b2d2516afd8a31e826adbe8b6d2ba19175e
Yingjie <1275963@gmail.com>
2016-03-05 10:55:31 +0800

    Runs with error

--- add_two_numbers/Solution.java
+++ add_two_numbers/Solution.java
@@ -5,16 +5,46 @@ import insertion_sort_list.ListNode;
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry;
-        int sum;
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null) {
+            int sum = 0;
+            
+            int l1val;
+            int l2val;
+            if(l1 == null)
+                l1val = 0;
+            else 
+                l1val = l1.val;
+            
+            if(l2 == null)
+                l2val = 0;
+            else
+                l2val = l2.val;
+            
+            sum = carry + l1val + l2val;
+            
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            if(l1 != null)  l1 = l1.next;
+            if(l2 != null)  l2 = l2.next;
+            carry = sum / 10;         
+        }
+        if(carry == 1)
+            p.next = new ListNode(1);
         
+        return dummy.next;       
     }
     
     
     public static void main(String[] args) {
-        ListNode test = ListNode.fromlist(new int [] {5, 5, 3, 2, 5, 5});
-        StdOut.println(test);
+        ListNode l1 = ListNode.fromlist(new int [] {5, 3, 3, 2, 5, 5});
+        StdOut.println("l1: " + l1);
 
         Solution sol = new Solution();
+        StdOut.println("Sum " + sol.addTwoNumbers(l1, l1));
     }
 }
