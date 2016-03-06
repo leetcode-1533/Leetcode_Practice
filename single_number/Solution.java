0261a5edfc27c2807314181ad94cf9afd9f1eebb
Yingjie <1275963@gmail.com>
2016-03-06 16:01:30 +0800

    Add some comments

--- single_number/Solution.java
+++ single_number/Solution.java
@@ -31,6 +31,24 @@ public class Solution {
   
     }
     
+//    hashmap version
+//    public int singleNumber(int[] nums) {
+//        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length);
+//        for(int item : nums) {
+//            if(map.get(item) == null) 
+//                map.put(item, 1);
+//            else 
+//                map.put(item, map.get(item) + 1);
+//        }
+//        
+//        for(Integer item : map.keySet()) {
+//            if(map.get(item) == 1)
+//                return item;
+//        }
+//        return 0;        
+//    }
+//    
+    
     public static void main(String[] args) {
         
     }
