312a213ca4b483d998ff4d8e3b12ecd52a141df4
Yingjie <1275963@gmail.com>
2016-03-11 11:16:38 +0800

    min_stack

---
+++ min_stack/Solution.java
@@ -0,0 +1,26 @@
+package min_stack;
+
+import java.util.*;
+public class Solution {
+    
+    private Stack<Integer> container = new Stack<Integer>();
+    private Stack<Integer> min = new Stack<Integer>();
+    
+    
+    public void push(int x) {
+        
+    }
+
+    public void pop() {
+        
+    }
+
+    public int top() {
+        
+    }
+
+    public int getMin() {
+        
+    }
+
+}
