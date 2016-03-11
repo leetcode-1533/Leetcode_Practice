e1ac9be53e804ea7e482207478ab2126b04a6b3a
Yingjie <1275963@gmail.com>
2016-03-11 11:21:04 +0800

    A bug at dupulicate

--- min_stack/Solution.java
+++ min_stack/Solution.java
@@ -8,19 +8,24 @@ public class Solution {
     
     
     public void push(int x) {
-        
+        container.push(x);
+        if(min.empty() || min.peek() > x) {
+            min.push(x);
+        }
     }
 
     public void pop() {
-        
+        int x = container.pop();
+        if(x == min.peek()) {
+            min.pop();
+        }
     }
 
     public int top() {
-        
+        return container.peek();
     }
 
     public int getMin() {
-        
+        return min.peek();       
     }
-
 }
