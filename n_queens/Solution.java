7f7d78f799be8f6ceac512d07aac0b7d89de4df9
Yingjie <1275963@gmail.com>
2016-03-11 16:59:43 +0800

    finished

--- n_queens/Solution.java
+++ n_queens/Solution.java
@@ -12,10 +12,13 @@ public class Solution {
             board = new boolean[n][n];
         }
         
-        Iterable<Board> perm(int level) {
+        ArrayList<Board> perm(int level) {
             ArrayList<Board> sol = new ArrayList<Board>();
             for(int i = 0; i < size; i++) {
                 Board temp = new Board(size);
+                for(int iter = 0; iter < board.length; iter++)
+                    temp.board[iter] = board[iter].clone();
+                
                 temp.board[level][i] = true;
                 if(temp.isValid())
                     sol.add(temp);
@@ -45,11 +48,11 @@ public class Solution {
         boolean checkDiag1() {
             int sum = 0;
             for(sum = 0; sum < size; sum++) {
-                System.out.printf("sum: %d\n", sum);
+//                System.out.printf("sum: %d\n", sum);
                 boolean flip = false;
                 for(int i = 0; i <= sum; i++) {
                     int j = sum - i;
-                    System.out.printf("i: %d, j: %d\n", i, j);
+//                    System.out.printf("i: %d, j: %d\n", i, j);
                     if(board[i][j] == true) {
                         if(flip == false)
                             flip = true;
@@ -61,11 +64,11 @@ public class Solution {
             int MINI = 1;
             for(; sum <= (size - 1) * 2; sum++) {
                 boolean flip = false;
-                System.out.printf("sum: %d\n", sum);
+//                System.out.printf("sum: %d\n", sum);
                 
                 for(int i = MINI; i < size; i++) {
                     int j = sum - i;
-                    System.out.printf("i: %d, j: %d\n", i, j);
+//                    System.out.printf("i: %d, j: %d\n", i, j);
                     if(board[i][j] == true) {
                         if(flip == false)
                             flip = true;
@@ -80,12 +83,12 @@ public class Solution {
         
         boolean checkDiag2() {
             for(int diff = 0; diff < size; diff++) {
-                System.out.printf("Diff: %d\n", diff);
+//                System.out.printf("Diff: %d\n", diff);
                 boolean flip1 = false;
                 boolean flip2 = false;
                 for(int i = 0; i < (size - diff); i++) {
                     int j = i + diff;
-                    System.out.printf("i: %d, j: %d\n", i, j);
+//                    System.out.printf("i: %d, j: %d\n", i, j);
                     if(board[i][j] == true) {
                         if(flip1 == true)
                             return false;
@@ -106,19 +109,25 @@ public class Solution {
         }       
     }
     
-    public void test() {
-        Board test = new Board(4);
-        test.checkDiag1();
-//        test.checkDiag2();
+    private void solveNqueens(int level, int n, Board gene, ArrayList<Board> container) {
+        if(n == level)
+            container.add(gene);
+        else {
+            for(Board item : gene.perm(level))
+                solveNqueens(level + 1, n, item, container);           
+        }        
     }
     
-    public List<List<String>> solveNQueens(int n) {
-        
+    public int totalNQueens(int n) {
+        ArrayList<Board> re = new ArrayList<Board>();
+        Board board = new Board(n);
+        solveNqueens(0, n, board, re);
+        return re.size();
     }
     
     public static void main(String[] args) {
         Solution sol = new Solution();
-        sol.test();
+//        System.out.println(sol.totalNQueens(4));
     }
 
 }
