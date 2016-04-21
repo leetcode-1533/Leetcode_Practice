d22a02d7bb3960fdfe13d385e14d778dd3d43ef5
Yingjie <1275963@gmail.com>
2016-04-21 16:36:18 +0800

    'Unique Path' Init

---
+++ unique_paths/Solution.java
@@ -0,0 +1,15 @@
+package unique_paths;
+
+/**
+ * Created by y1275963 on 4/21/16.
+ */
+public class Solution {
+
+    public int uniquePaths(int m, int n) {
+
+    }
+
+    public void static main(String[] args) {
+
+    }
+}
