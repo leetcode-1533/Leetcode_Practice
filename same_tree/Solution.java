ffacfa839e532ab4561aa152f6c484e7ce8d1e59
Yingjie <1275963@gmail.com>
2016-04-21 19:26:07 +0800

    'Same Tree' iter method

--- same_tree/Solution.java
+++ same_tree/Solution.java
@@ -1,18 +1,33 @@
 package same_tree;
 
 import utility.TreeNode;
+import java.util.Stack;
 /**
  * Created by y1275963 on 4/21/16.
  */
 public class Solution {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
+        if (p == q)
+            return true;
+        else {
+            if (p == null || q == null) // either of which
+                return false;
+            else {
+                return (p.val == q.val
+                        && isSameTree(p.left, q.left)
+                        && isSameTree(p.right, q.right));
+            }
+        }
     }
-    
+
     public static void main(String[] args) {
+        TreeNode tree1 = TreeNode.build(new Integer[] {1,2,3,4,null,null,5});
+        TreeNode tree2 = TreeNode.build(new Integer[] {1,2,3,4,null,null,4});
+
+
         Solution sol = new Solution();
-        TreeNode tree = TreeNode.build(new Integer[] {1,2,3,4,null,null,5});
-        tree.print();
+        System.out.println(sol.isSameTree(tree1, tree2));
+
     }
 }
