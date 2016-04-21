58276ff3bd0d86e3672ece9ccb7f9b6d00d17cf3
Yingjie <1275963@gmail.com>
2016-04-21 10:54:42 +0800

    Queue too slow, trying with array

--- climbing_stairs/Solution.java
+++ climbing_stairs/Solution.java
@@ -1,7 +1,36 @@
 package climbing_stairs;
 
+import java.util.concurrent.ArrayBlockingQueue;
 /**
  * Created by y1275963 on 4/21/16.
  */
 public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            int[] ans = new int[]{1, 2};
+            return ans[n - 1];
+        } else {
+            ArrayBlockingQueue<Integer> que = new ArrayBlockingQueue<Integer>(3);
+            que.add(1);
+            que.add(2);
+
+            int counter = 2;
+            int ans = 0;
+            while (counter != n) {
+                ans = 0;
+                for(Integer item : que) {
+                    ans += item;
+                }
+                que.poll();
+                que.add(ans);
+                counter++;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.climbStairs(1));
+    }
 }
