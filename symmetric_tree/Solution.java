35992ab80afeaaa17bfa5262241241f349f0a3bf
Yingjie <1275963@gmail.com>
2016-04-21 20:14:41 +0800

    'Symmetric_tree': Iterative Method using queue

--- symmetric_tree/Solution.java
+++ symmetric_tree/Solution.java
@@ -1,16 +1,17 @@
 package symmetric_tree;
 
 import utility.TreeNode;
-
+import java.util.Queue;
+import java.util.ArrayDeque;
 /**
  * Created by y1275963 on 4/21/16.
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root == null || isSymmetricTree(root.left, root.right);
+        return root == null || isSymmetricTree2(root.left, root.right);
     }
 
-    public boolean isSymmetricTree(TreeNode p, TreeNode q) {
+    public boolean isSymmetricTree1(TreeNode p, TreeNode q) {
         if (p == q)
             return true;
         else {
@@ -18,14 +19,46 @@ public class Solution {
                 return false;
             else {
                 return (p.val == q.val
-                        && isSymmetricTree(p.left, q.right)
-                        && isSymmetricTree(p.right, q.left));
+                        && isSymmetricTree1(p.left, q.right)
+                        && isSymmetricTree1(p.right, q.left));
             }
         }
     }
 
+    public boolean isSymmetricTree2(TreeNode p, TreeNode q) {
+        if (p == q)
+            return true;
+        if (p == null || q == null)
+            return false;
+
+        Queue<TreeNode> quel = new ArrayDeque<TreeNode>();
+        Queue<TreeNode> quer = new ArrayDeque<TreeNode>();
+        quel.add(p);
+        quer.add(q);
+        while (!quel.isEmpty()) {
+            TreeNode le = quel.poll();
+            TreeNode ri = quer.poll();
+            if (le.val != ri.val)
+                return false;
+            else if ((enqueue(quel, le.left, quer, ri.right)
+                    && enqueue(quel, le.right, quer, ri.left)) == false)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean enqueue(Queue<TreeNode> quel, TreeNode nl, Queue<TreeNode> quer, TreeNode nr) {
+        if(nl == null && nr == null)
+            return true;
+        if(nl == null || nr == null)
+            return false;
+        quel.add(nl);
+        quer.add(nr);
+        return true;
+    }
+
     public static void main(String[] args) {
-        TreeNode tree1 = TreeNode.build(new Integer[] {1,2,2,4,null,null,4});
+        TreeNode tree1 = TreeNode.build(new Integer[] {1, 2, 2});
         Solution sol = new Solution();
         System.out.println(sol.isSymmetric(tree1));
     }
