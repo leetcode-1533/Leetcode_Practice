6c7beeb57b60d758d1eb4d13f0f7864a0c0f4be1
Yingjie <1275963@gmail.com>
2016-05-01 11:38:15 +0800

    'balanced_binary_tree' accepted

--- balanced_binary_tree/Solution.java
+++ balanced_binary_tree/Solution.java
@@ -5,10 +5,9 @@ import utility.TreeNode;
  * Created by y1275963 on 5/1/16.
  */
 public class Solution {
-//    public boolean isBalanced(TreeNode root) {
-//        return false;
-//
-//    }
+    public boolean isBalanced(TreeNode root) {
+        return depthof(root) != -1;
+    }
 
     private int depthof(TreeNode node) {
         int cur_dl = 0;
@@ -28,7 +27,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        TreeNode tree1 = TreeNode.build(new Integer[] {1});
+        TreeNode tree1 = TreeNode.build(new Integer[] {1,2, null, 3});
         Solution sol = new Solution();
         System.out.println(sol.depthof(tree1));
     }
