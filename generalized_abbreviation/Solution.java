f67c66f5d32558cfba907020aec043d87f651340
Yingjie <1275963@gmail.com>
2016-05-24 15:00:55 +0800

    'generalized_abbreviation' 1/2

--- generalized_abbreviation/Solution.java
+++ generalized_abbreviation/Solution.java
@@ -1,9 +1,31 @@
 package generalized_abbreviation;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.StringBuilder;
 /**
  * Created by y1275963 on 5/24/16.
  */
 public class Solution {
+
+    public List<String> generateAbbreviations(String word) {
+        int N = word.length();
+        List<String> ans = new ArrayList<>((int) Math.pow(2, N));
+
+        for (int i = 0; i < Math.pow(2, N); i++) {
+            boolean[] mode = Solution.toBinary(i, N);
+            Character[] ori = new Character[N];
+            for (int j = 0; j < N; j++) { // string into character array
+                if (mode[j])
+                    ori[j] = new Character(word.charAt(j));
+                else
+                    ori[j] = null;
+            }
+        }
+        return ans;
+    }
+
+
     private static boolean[] toBinary(int number, int base) {
         final boolean[] ret = new boolean[base];
         for (int i = 0; i < base; i++) {
@@ -13,11 +35,12 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        boolean[] test = Solution.toBinary(10, 4);
-        for (boolean item : test) {
-            System.out.print(item);
-            System.out.print('\t');
-        }
+        Solution test = new Solution();
+        test.generateAbbreviations("WORD");
+//        boolean[] test = Solution.toBinary(10, 4);
+//        for (boolean item : test) {
+//            System.out.print(item);
+//            System.out.print('\t');
+//        }
     }
-
 }
