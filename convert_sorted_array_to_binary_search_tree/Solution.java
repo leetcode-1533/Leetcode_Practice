f7c0a0656c63561711dc61e9d138573794ae216d
Yingjie <1275963@gmail.com>
2016-05-24 21:11:41 +0800

    convert_sorted_array_to_binary_search_tree odd number fixed

---
+++ convert_sorted_array_to_binary_search_tree/Solution.java
@@ -0,0 +1,40 @@
+package convert_sorted_array_to_binary_search_tree;
+
+/**
+ * Created by y1275963 on 5/24/16.
+ */
+import utility.TreeNode;
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int lo, int hi) {
+        if (lo == hi)
+            return new TreeNode(nums[lo]);
+        else if (hi - lo == 1) {
+            TreeNode root = new TreeNode(nums[hi]);
+            root.left = new TreeNode(nums[lo]);
+            return root;
+        }
+        else {
+            int root_loc = (int) Math.ceil((hi + lo) / 2);
+            TreeNode root = new TreeNode(nums[root_loc]);
+            root.left = dfs(nums, lo, root_loc - 1);
+            root.right = dfs(nums, root_loc + 1, hi);
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = TreeNode.build(new Integer[] {1,2,3,4,null,null,5});
+        tree.print();
+
+        System.out.println();
+
+        Solution test = new Solution();
+        TreeNode re = test.sortedArrayToBST(new int[] {1, 3, 4, 5, 12, 15, 16});
+        re.print();
+    }
+}
