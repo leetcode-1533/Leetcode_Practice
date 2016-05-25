4f646558083125e6c0e3815059b97acdcb8d3ce8
Yingjie <1275963@gmail.com>
2016-05-25 11:44:47 +0800

    dfs v2, use only one hashMap, save half the memory at the same speed

--- clone_graph/Solution.java
+++ clone_graph/Solution.java
@@ -16,25 +16,33 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Solution {
+//    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+//        if (node == null)
+//            return null;
+//
+//        HashMap<Integer, ArrayList<Integer>> labelMap = new HashMap<>();
+//        dfs(node, labelMap);
+//
+//        HashMap<Integer, UndirectedGraphNode> newNodeMap = new HashMap<>();
+//        for (int item : labelMap.keySet()) {
+//            newNodeMap.put(item, new UndirectedGraphNode(item));
+//        }
+//
+//        for (int item : labelMap.keySet()) {
+//            UndirectedGraphNode newNode = newNodeMap.get(item);
+//            for (int newNode_nei_label : labelMap.get(item)) {
+//                newNode.neighbors.add(newNodeMap.get(newNode_nei_label));
+//            }
+//        }
+//        return newNodeMap.get(node.label);
+//    }
+
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if (node == null)
             return null;
 
-        HashMap<Integer, ArrayList<Integer>> labelMap = new HashMap<>();
-        dfs(node, labelMap);
-
-        HashMap<Integer, UndirectedGraphNode> newNodeMap = new HashMap<>();
-        for (int item : labelMap.keySet()) {
-            newNodeMap.put(item, new UndirectedGraphNode(item));
-        }
-
-        for (int item : labelMap.keySet()) {
-            UndirectedGraphNode newNode = newNodeMap.get(item);
-            for (int newNode_nei_label : labelMap.get(item)) {
-                newNode.neighbors.add(newNodeMap.get(newNode_nei_label));
-            }
-        }
-        return newNodeMap.get(node.label);
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> set = new HashMap<>();
+        return dfs2(node, set);
     }
 
     private void dfs(UndirectedGraphNode Node, HashMap<Integer, ArrayList<Integer>> set) {
@@ -46,6 +54,22 @@ public class Solution {
                 nei_labels.add(nei.label);
                 dfs(nei, set);
             }
+        }
+    }
+
+    private UndirectedGraphNode dfs2(UndirectedGraphNode Node, HashMap<UndirectedGraphNode, UndirectedGraphNode> set) {
+        if (!set.containsKey(Node)) {
+            UndirectedGraphNode newNode = new UndirectedGraphNode(Node.label);
+            set.put(Node, newNode);
+
+            for (UndirectedGraphNode neigh : Node.neighbors) {
+                UndirectedGraphNode new_neigh = dfs2(neigh, set);
+                newNode.neighbors.add(new_neigh);
+            }
+
+            return newNode;
+        } else {
+            return set.get(Node);
         }
     }
 
