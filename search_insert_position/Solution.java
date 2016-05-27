2307f4d23ee06aa03af4eceab738b1b4ea9ad599
Yingjie <1275963@gmail.com>
2016-05-27 12:09:23 +0800

    search_insert_position

--- search_insert_position/Solution.java
+++ search_insert_position/Solution.java
@@ -5,18 +5,47 @@ package search_insert_position;
  */
 public class Solution {
     public int searchInsert(int[] nums, int target) {
-        return 0;
+        if (nums == null || nums.length == 0)
+            return 0;
+        if (nums.length == 1) {
+            if (target <= nums[0])
+                return 0;
+            else
+                return 1;
+        }
 
+
+        return binarySearch(nums, 0, nums.length - 1, target);
     }
 
     private int binarySearch(int[] nums, int lo, int hi, int target) {
-        return 0;
-
+        if (hi - lo > 1) {
+            int mid = (int) Math.ceil((lo + hi) / 2.0);
+            if (target == nums[mid])
+                return mid;
+            if (target > nums[mid])
+                return binarySearch(nums, mid, hi, target);
+            else // (target < nums[mid])
+                return binarySearch(nums, lo, mid, target);
+        } else { // hi - lo == 1
+            if (target <= nums[lo]) // equality check
+                return lo;
+            else if (target > nums[hi])
+                return hi + 1;
+            else
+                return hi;
+        }
     }
 
     public static void main(String[] args) {
         int a = 3;
         int b = 2;
-        System.out.println((int) Math.ceil((double) a/b));
+        int midab =  (int) Math.ceil((double) a/b);
+//        System.out.println(midab);
+
+        int[] nums = new int[] {1, 3};
+        Solution sol = new Solution();
+        int target = 1;
+        System.out.println(sol.searchInsert(nums, target));
     }
 }
