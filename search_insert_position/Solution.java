ac2a8fe01b8d88a4db8155ee5aedfe5fc62aabfc
Yingjie <1275963@gmail.com>
2016-05-27 11:49:14 +0800

    Test for even length array mid-indexing

---
+++ search_insert_position/Solution.java
@@ -0,0 +1,22 @@
+package search_insert_position;
+
+/**
+ * Created by y1275963 on 5/27/16.
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+
+    }
+
+    private int binarySearch(int[] nums, int lo, int hi, int target) {
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        System.out.println((int) Math.ceil((double) a/b));
+    }
+}
