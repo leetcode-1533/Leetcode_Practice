7877c0524acb80c580d38fdde7d9f3cc5b50122b
Yingjie <1275963@gmail.com>
2016-05-28 19:00:46 +0800

    Timing compare hanoi tower

--- hanoi_tower/Solution.java
+++ hanoi_tower/Solution.java
@@ -9,21 +9,21 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class Solution {
-    public static int printprecedure(int n) {
+    public static int mem_num_precedure(int n) {
         if (n < 1)
             throw new IllegalArgumentException();
         Map<Integer, Integer> mem = new HashMap<>();
-        return precedure(n, 'A', 'B', 'C', mem);
+        return mem_precedure(n, 'A', 'B', 'C', mem);
     }
 
-    private static int precedure(int n, char beg, char aux, char end, Map<Integer, Integer> mem) {
+    private static int mem_precedure(int n, char beg, char aux, char end, Map<Integer, Integer> mem) {
         int count = 0;
         if (n > 1) {
             int temp = 0;
             if (mem.containsKey(n))
                 temp = mem.get(n);
             else
-                temp = precedure(n - 1, beg, end, aux, mem);
+                temp = mem_precedure(n - 1, beg, end, aux, mem);
 
             count = count + temp * 2 + 1;
 
@@ -36,8 +36,41 @@ public class Solution {
         }
         return count;
     }
+    /*
+    * BF method
+     */
+    public static int printprecedure(int n) {
+        if (n < 1)
+            throw new IllegalArgumentException();
+        return precedure(n, 'A', 'B', 'C');
+    }
+
+    private static int precedure(int n, char beg, char aux, char end) {
+        int count = 0;
+        if (n > 1) {
+            count += precedure(n - 1, beg, end, aux);
+            //precedure(1, beg, aux, end);
+            count++;
+            count += precedure(n - 1, aux, beg, end);
+        } else { // n == 1
+            return 1;
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(Solution.printprecedure(5));
+
+        int n = 3;
+
+        long bf_start = System.nanoTime();
+            Solution.printprecedure(n);
+        long bf_end = System.nanoTime();
+        System.out.printf("Brute-Force  : %f milliseconds\n", (bf_end - bf_start) / 1000000.0);
+
+        long mem_start = System.nanoTime();
+            Solution.mem_num_precedure(n);
+        long mem_end = System.nanoTime();
+        System.out.printf("Memorization : %f milliseconds\n", (mem_end - mem_start) / 1000000.0);
     }
 }
