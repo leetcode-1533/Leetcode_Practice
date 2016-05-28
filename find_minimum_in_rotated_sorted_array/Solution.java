171d91b130e38187d3e452b683f27c00b47ae4d7
Yingjie <1275963@gmail.com>
2016-05-28 17:32:20 +0800

    code summery

--- find_minimum_in_rotated_sorted_array/Solution.java
+++ find_minimum_in_rotated_sorted_array/Solution.java
@@ -4,6 +4,9 @@ package find_minimum_in_rotated_sorted_array;
  * Created by y1275963 on 5/27/16.
  */
 public class Solution {
+    /*
+     without duplicate, log(n)
+     */
     public int findMin(int[] nums) {
         int lo = 0;
         int hi = nums.length - 1;
@@ -17,16 +20,38 @@ public class Solution {
                 lo = mid;
             else if (nums[mid] < nums[hi])
                 hi = mid;
-            else {
-                lo = lo + 1;
-            }
         }
         return Math.min(nums[lo], nums[hi]);
+        // not rotated, return nums[lo] only happen when n=2
+        // rotated, return nums[hi]
     }
+
+    /*
+    with duplicate, o(n) worst case
+     */
+//    public int findMin(int[] nums) {
+//        int lo = 0;
+//        int hi = nums.length - 1;
+//        int mid;
+//        while (hi - lo > 1) {
+//            if (nums[lo] < nums[hi]) // if not rotated
+//                return nums[lo];
+//
+//            mid = (lo + hi) / 2;
+//            if (nums[lo] < nums[mid])
+//                lo = mid;
+//            else if (nums[mid] < nums[hi])
+//                hi = mid;
+//            else {
+//                lo = lo + 1;
+//            }
+//        }
+//        return Math.min(nums[lo], nums[hi]);
+//    }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 //        System.out.println(sol.findMin(new int[] {4,5,6,7,0,1,2}));
-        System.out.println(sol.findMin(new int[] {1, 3}));
+        System.out.println(sol.findMin(new int[] {3, 1}));
     }
 }
