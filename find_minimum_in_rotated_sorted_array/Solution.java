ea17d62d10edfbcbe3c00504faa34a55001bb651
Yingjie <1275963@gmail.com>
2016-05-28 17:17:12 +0800

    'find_minimum_in_rotated_sorted_array' using binary search

--- find_minimum_in_rotated_sorted_array/Solution.java
+++ find_minimum_in_rotated_sorted_array/Solution.java
@@ -5,6 +5,25 @@ package find_minimum_in_rotated_sorted_array;
  */
 public class Solution {
     public int findMin(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        int mid;
+        while (hi - lo > 1) {
+            if (nums[lo] < nums[hi]) // if not rotated
+                return nums[lo];
 
+            mid = (lo + hi) / 2;
+            if (nums[lo] < nums[mid])
+                lo = mid;
+            else if (nums[mid] < nums[hi])
+                hi = mid;
+        }
+        return nums[hi];
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+//        System.out.println(sol.findMin(new int[] {4,5,6,7,0,1,2}));
+        System.out.println(sol.findMin(new int[] {1, 2}));
     }
 }
