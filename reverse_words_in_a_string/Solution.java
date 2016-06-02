e07597a31d54b14ab3775cfd6832538770e754c4
Yingjie <1275963@gmail.com>
2016-06-02 10:10:04 +0800

    Reverse string using nondeterministic state machine

--- reverse_words_in_a_string/Solution.java
+++ reverse_words_in_a_string/Solution.java
@@ -5,8 +5,25 @@ package reverse_words_in_a_string;
  */
 public class Solution {
 
+    public String reverseWords(String s) {
+        String[] parts = s.split(" ");
+        StringBuilder re = new StringBuilder();
+        for (int i = parts.length - 1; i >= 0; i--) {
+            if (parts[i].length() > 0) {
+                re.append(parts[i]);
+                re.append(' ');
+            }
+        }
+        String ans = re.toString();
+        return ans.length() == 0 ? ans : ans.substring(0, ans.length() - 1);
+    }
 
     public static void main(String[] args) {
+        String str1 = " 1";
+        String str2 = "the sky is blue";
+        Solution sol = new Solution();
+        System.out.print(sol.reverseWords(str1));
+        System.out.print('@');
 
     }
 }
