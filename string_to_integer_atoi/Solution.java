22288e4938be836ae9a9ef8879dcc3a547c5a6d2
Yingjie <1275963@gmail.com>
2016-06-02 18:43:48 +0800

    Started atoi

---
+++ string_to_integer_atoi/Solution.java
@@ -0,0 +1,20 @@
+package string_to_integer_atoi;
+
+/**
+ * Created by y1275963 on 6/2/16.
+ *
+ * Implement atoi to convert a string to an integer.
+
+ Hint: Carefully consider all possible input cases.
+ If you want a challenge,
+ please do not see below and ask yourself what are the possible input cases.
+
+ Notes: It is intended for this problem to be specified vaguely(ie, no given input specs).
+ You are responsible to gather all the input requirements up front.
+
+ */
+public class Solution {
+    public int myAtoi(String str) {
+
+    }
+}
