265f3a7690827acd25d624dbcc39066e24026b1c
Yingjie <1275963@gmail.com>
2016-06-04 21:13:39 +0800

    log previous location with hashtable

--- longest_substring_without_repeating_characters/Solution.java
+++ longest_substring_without_repeating_characters/Solution.java
@@ -3,38 +3,40 @@ package longest_substring_without_repeating_characters;
 /**
  * Created by y1275963 on 6/4/16.
  */
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> counter = new HashSet<>();
+        Map<Character, Integer> hist = new HashMap<>();
+        int starti = 0;
         int best = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-
-            if (counter.contains(current)) {
-                if (counter.size() > best)
-                    best = counter.size();
-                counter.clear();
+        for (int j = 0; j < s.length(); j++) {
+            if (hist.containsKey(s.charAt(j))) {
+                int previous_loc = hist.get(s.charAt(j));
+                starti = Math.max(previous_loc + 1, starti);
             }
-
-            counter.add(current);
+            hist.put(s.charAt(j), j);
+            best = Math.max(best, j - starti + 1);
         }
-        return counter.size() > best ? counter.size() : best;
+
+        return best;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        sol.lengthOfLongestSubstring("dvdf");
+        sol.lengthOfLongestSubstring("abcabcbb");
 
         List<String> testSet = new ArrayList<String>();
         testSet.add("c");
-        testSet.add("abcabcbb");
+        testSet.add("dvdf");
+        testSet.add("abba");
         testSet.add("bbbbb");
         testSet.add("pwwkew");
+        testSet.add("abcabcbb");
+
         for (String item : testSet)
             System.out.printf("%s of length %d\n", item ,sol.lengthOfLongestSubstring(item));
     }
