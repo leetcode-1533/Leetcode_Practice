8e94f6583aa02ff6a28a0c333b10f9b66a42c092
Yingjie <1275963@gmail.com>
2016-06-13 08:40:58 +0800

    Code optimization

--- read_n_characters_given_read4/Solution.java
+++ read_n_characters_given_read4/Solution.java
@@ -15,18 +15,16 @@ public class Solution extends Reader4 {
      */
     public int read(char[] buf, int n) {
         int realn = 0;
+        int readc = 4;
 
-        while (realn < n) {
+        while (readc == 4 && realn < n) {
             char[] buf4 = new char[4];
-            int readc = read4(buf4);
+            readc = read4(buf4);
             int temp = Math.min(readc, n - realn);
 
             System.arraycopy(buf4 /* src */, 0 /* srcPos */,
                     buf /* dest */, realn /* destPos */, temp/* length */);
             realn += temp;
-
-            if (readc < 4)
-                break;
         }
         return realn;
     }
