f39c08f99b35ee563a139c33c322071cf17f586d
Yingjie <1275963@gmail.com>
2016-06-14 12:23:32 +0800

    Linked List as a separate utility

--- lru_cache/LRUCache.java
+++ lru_cache/LRUCache.java
@@ -4,70 +4,84 @@ package lru_cache;
  * Created by y1275963 on 6/13/16.
  */
 import java.util.HashMap;
-
+import java.util.Map;
 
 public class LRUCache {
     private int size;
     private LinkedList list = new LinkedList();
+    private Map<Integer, LinkedList.Node> map = new HashMap<>();
 
-    private class LinkedList {
-        public class Node {
-            /*
-            head   <- before Node after ->  tail
-             */
-            private final int value;
-            private Node before;
-            private Node after;
-
-            private Node (int val, Node be, Node af) {
-                value = val;
-                before = be;
-                after = af;
-            }
-        }
-
-        private Node head = null, tail = null;
-        private int size = 0;
-
-        private Node offer(int value) {
-            // Add to the tail
-            if (head == null && tail == null) {
-                head = new Node(value, null, null);
-                tail = head;
-            } else {
-                Node newnode = new Node(value, tail, null);
-                tail.after = newnode;
-                tail = newnode;
-            }
-            return tail;
-        }
-
-        private void delHead() {
-            head = head.after;
-        }
-
-        private void addCount(Node node) {
-            // move to the tail
-            tail.after = node;
-            tail = node;
-        }
+    public LRUCache(int capacity) {
+        size = capacity;
     }
 
     public int get(int key) {
-        return 0;
+        if (map.containsKey(key)) {
+            int re = map.get(key);
+            list.addCount(key);
+            return re;
+        } else {
+            return -1;
+        }
     }
 
     public void set(int key, int value) {
-//        return 0;
+        if (map.size() == size) {
+
+        }
+
     }
 
     public static void main(String[] args) {
-        LRUCache sol = new LRUCache();
-        LRUCache.LinkedList.Node node1 = sol.list.offer(1);
-        LRUCache.LinkedList.Node node2 = sol.list.offer(2);
-        LRUCache.LinkedList.Node node3 = sol.list.offer(3);
-        LRUCache.LinkedList.Node node4 = sol.list.offer(4);
-        LRUCache.LinkedList.Node node5 = sol.list.offer(5);
-        sol.list.addCount(node3);
+    }
+}
+
+class LinkedList {
+    public class Node {
+        /*
+        head   <- before Node after ->  tail
+         */
+        private final int key;
+        private final int value;
+        private Node before;
+        private Node after;
+
+        public Node (int init_key, int init_val, Node be, Node af) {
+            value = init_val;
+            key = init_key;
+            before = be;
+            after = af;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node head = null, tail = null;
+
+    public Node offer(int value) {
+        // Add to the tail
+        if (head == null && tail == null) {
+            head = new Node(value, null, null);
+            tail = head;
+        } else {
+            Node newnode = new Node(value, tail, null);
+            tail.after = newnode;
+            tail = newnode;
+        }
+        return tail;
+    }
+
+    public Node poll() {
+        Node temp = head;
+        head = head.after;
+        return temp;
+    }
+
+    public void addCount(Node node) {
+        // move to the tail
+        tail.after = node;
+        tail = node;
     }
 }
