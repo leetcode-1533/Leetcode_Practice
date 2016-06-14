193357fb6a8c0d37e640f68d97219e6f001badd3
Yingjie <1275963@gmail.com>
2016-06-14 14:22:38 +0800

    Moved back into inner class, two classes are not allowed

--- lru_cache/LRUCache.java
+++ lru_cache/LRUCache.java
@@ -34,6 +34,74 @@ public class LRUCache {
         map.put(key, newnode);
     }
 
+    private class LinkedList {
+        public class Node {
+            /*
+            head   <- before Node after ->  tail
+             */
+            private final int key;
+            private final int value;
+            private Node before;
+            private Node after;
+
+            public Node (int init_key, int init_val, Node be, Node af) {
+                value = init_val;
+                key = init_key;
+                before = be;
+                after = af;
+            }
+
+            public int getKey() {
+                return key;
+            }
+
+            public int getValue() {
+                return value;
+            }
+        }
+
+        private Node head = null, tail = null;
+
+        public Node offer(int key, int value) {
+            // Add to the tail
+            if (head == null && tail == null) {
+                head = new Node(key, value, null, null);
+                tail = head;
+            } else {
+                Node newnode = new Node(key, value, tail, null);
+                tail.after = newnode;
+                tail = newnode;
+            }
+            return tail;
+        }
+
+        public Node poll() {
+            Node temp = head;
+            head = head.after;
+            return temp;
+        }
+
+        public void addCount(Node node) {
+            // move to the tail
+            if (node != tail) {
+                if (node == head)
+                    head = head.after;
+
+                Node be = node.before;
+                Node af = node.after;
+
+                if (be != null) // in case node is the head
+                    be.after = af;
+                af.before = be;
+
+                tail.after = node;
+                node.before = tail;
+                tail = node;
+                tail.after = null;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LRUCache sol = new LRUCache(5);
         int[] keySet = new int[] {3, 4, 5, 6, 7};
@@ -46,72 +114,6 @@ public class LRUCache {
         sol.get(3);
         sol.set(8, 8);
     }
-}
 
-class LinkedList {
-    public class Node {
-        /*
-        head   <- before Node after ->  tail
-         */
-        private final int key;
-        private final int value;
-        private Node before;
-        private Node after;
 
-        public Node (int init_key, int init_val, Node be, Node af) {
-            value = init_val;
-            key = init_key;
-            before = be;
-            after = af;
-        }
-
-        public int getKey() {
-            return key;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-    private Node head = null, tail = null;
-
-    public Node offer(int key, int value) {
-        // Add to the tail
-        if (head == null && tail == null) {
-            head = new Node(key, value, null, null);
-            tail = head;
-        } else {
-            Node newnode = new Node(key, value, tail, null);
-            tail.after = newnode;
-            tail = newnode;
-        }
-        return tail;
-    }
-
-    public Node poll() {
-        Node temp = head;
-        head = head.after;
-        return temp;
-    }
-
-    public void addCount(Node node) {
-        // move to the tail
-        if (node != tail) {
-            if (node == head)
-                head = head.after;
-
-            Node be = node.before;
-            Node af = node.after;
-
-            if (be != null) // in case node is the head
-                be.after = af;
-            af.before = be;
-
-            tail.after = node;
-            node.before = tail;
-            tail = node;
-            tail.after = null;
-        }
-    }
 }
