912602890a20014780be890449eaa7948de21110
Yingjie <1275963@gmail.com>
2016-06-14 16:44:24 +0800

    Started implementing trie

---
+++ implement_trie_prefix_tree/Trie.java
@@ -0,0 +1,40 @@
+package implement_trie_prefix_tree;
+
+/**
+ * Created by y1275963 on 6/14/16.
+ */
+class TrieNode {
+    // Initialize your data structure here.
+    public TrieNode() {
+
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        return true;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        return true;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
