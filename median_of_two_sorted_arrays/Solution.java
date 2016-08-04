f4e30efa5963258b5ec7ed74a2243bba79be0e36
Yingjie <1275963@gmail.com>
2016-08-04 14:43:40 +0800

    O(N) by merge working

--- median_of_two_sorted_arrays/Solution.java
+++ median_of_two_sorted_arrays/Solution.java
@@ -4,11 +4,25 @@ package median_of_two_sorted_arrays;
  * Created by y1275963 on 8/4/16.
  */
 public class Solution {
+    private int i, j;
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         return 0;
     }
 
-    private int i, j;
+    public double findMedianSortedArrays_merge(int[] nums1, int[] nums2) {
+        i = 0;
+        j = 0;
+
+        int target = (nums1.length + nums2.length + 1) / 2 - 1;
+        while (i + j < target)
+            next(nums1, nums2);
+
+        if ((nums1.length + nums2.length) % 2 == 0)
+            return (next(nums1, nums2) + next(nums1, nums2)) / 2.0;
+        else
+            return next(nums1, nums2);
+    }
 
     private int next(int[] nums1, int[] nums2) {
         if (i == nums1.length)
@@ -22,11 +36,16 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] test1 = new int[]{1, 2, 3, 4};
-        int[] test2 = new int[]{4, 5, 6, 7};
         Solution sol = new Solution();
+        int[] test1 = new int[]{1};
+        int[] test2 = new int[]{};
 
-        for (int i = 0; i < test1.length + test2.length; i++)
-            System.out.println(sol.next(test1, test2));
+        System.out.println(sol.findMedianSortedArrays_merge(test1, test2));
+
+
+//
+//        for (int i = 0; i < test1.length + test2.length; i++)
+//            System.out.println(sol.next(test1, test2));
+
     }
 }
