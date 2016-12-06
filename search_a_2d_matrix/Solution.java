85f46c8ff3dde097f0131c00b62c484680f9f662
Yingjie <1275963@gmail.com>
2016-12-06 00:16:07 -0500

    Removes comments

--- search_a_2d_matrix/Solution.java
+++ search_a_2d_matrix/Solution.java
@@ -10,28 +10,6 @@ public class Solution {
         return re == target;
     }
 
-//    public static int binarySearch(int[] test, int target, int start, int end) {
-//        /*
-//        Binary search a 1d array
-//        return value
-//        if not found, return smaller nearest value
-//         */
-//        int med = start;
-//        while((end - start) > 1 && target != test[med]) {
-//            med = (start +  end) / 2; // should be an int
-//            if (target > test[med])
-//                start = med;
-//            else
-//                end = med;
-//        }
-//        if (test[med] == target)
-//            return test[med];
-//        else if (test[end] < target) // out of bound
-//            return test[end];
-//        else
-//            return test[start];
-//    }
-
     private static int binarySearch_byrows(int[][] matrix, int col, int target, int start, int end) {
       /*
         Binary search a 2d array by rows
@@ -78,7 +56,6 @@ public class Solution {
 
 
     public static void main(String[] args) {
-//        Solution sol = new Solution();
         // 2d cases
         int[][] matrix_test = new int[][]{{1, 3, 5, 7}, {10, 11, 16 ,20}, {23, 30, 34, 60}};
         System.out.println(Solution.searchMatrix(matrix_test, 12));
@@ -88,29 +65,6 @@ public class Solution {
 
         matrix_test = new int[][]{{1}, {3}};
         System.out.println(Solution.searchMatrix(matrix_test, 1));
-
-//        int re = sol.binarySearch_byrows(matrix_test, 1, 29, 0, matrix_test.length - 1);
-//        System.out.println(re);
-
-//        int re = sol.binarySearch_bycolumns(matrix_test, 0, 6, 0, matrix_test[0].length - 1);
-//        System.out.println(re);
-
-//
-//        int[] test = new int[]{1, 2, 32, 50, 80};
-//        for (int item : test) {
-//            System.out.print(item);
-//            System.out.print('\t');
-//        }
-//        System.out.println();
-//
-//        int ans = sol.binarySearch(test, -5, 0, test.length-1);
-//        System.out.println("-5: " + ans);
-//        ans = sol.binarySearch(test, 1, 0, test.length-1);
-//        System.out.println("1: " + ans);
-//        ans = sol.binarySearch(test, 20, 0, test.length-1);
-//        System.out.println("20: " + ans);
-//        ans = sol.binarySearch(test, 90, 0, test.length-1);
-//        System.out.println("90: " + ans);
 
     }
 }
