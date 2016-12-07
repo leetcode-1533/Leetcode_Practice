3475b3953271d338c0d0679f1baf97124d36d09b
Yingjie <1275963@gmail.com>
2016-12-07 11:56:55 -0500

    Search a 2d matrix II PASSED
    Using a different approach, started search from top right.
    2D Matrix I can be done in similiar approach, but using binary search it was log(m) + log(n)
    
    Currently running time is m + n.

--- search_a_2d_matrix_ii/Solution.java
+++ search_a_2d_matrix_ii/Solution.java
@@ -2,101 +2,20 @@
 package search_a_2d_matrix_ii;
 
 public class Solution {
-    private static int binarySearch_bydiagonal(int[][] matrix, int target, int start, int end, int row_shift, int col_shift) {
-        /*
-        Binary search a 2d array by diagonal
-        return index
-        if not found, return smaller nearest value
-         */
-        int med = start;
-        while ((end - start) > 1 && target != matrix[med + row_shift][med + col_shift]) {
-            med = (start + end) / 2; // should be an int
-            if (target > matrix[med + row_shift][med + col_shift])
-                start = med;
-            else
-                end = med;
-        }
-
-        if (matrix[med + row_shift][med + col_shift] == target)
-            return med;
-        else if (matrix[end + row_shift][end + col_shift] <= target) // maximum value out of bound
-            return end;
-        else
-            return start;
-    }
-
-    private static int binarySearch_byrows(int[][] matrix, int col, int target, int start, int end) {
-      /*
-        Binary search a 2d array by rows
-        return value
-        if not found, return smaller nearest value
-         */
-        int med = start;
-        while((end - start) > 1 && target != matrix[med][col]) {
-            med = (start +  end) / 2; // should be an int
-            if (target > matrix[med][col])
-                start = med;
-            else
-                end = med;
-        }
-
-        if (matrix[med][col] == target)
-            return matrix[med][col];
-        else if (matrix[end][col] <= target) // maximum value out of bound
-            return matrix[end][col];
-        else
-            return matrix[start][col];
-    }
-
-    private static int binarySearch_bycolumns(int[][] matrix, int row, int target, int start, int end) {
-      /*
-        Binary search a 2d array by rows
-        return value
-        if not found, return smaller nearest value
-         */
-        int med = start;
-        while((end - start) > 1 && target != matrix[row][med]) {
-            med = (start +  end) / 2; // should be an int
-            if (target > matrix[row][med])
-                start = med;
-            else
-                end = med;
-        }
-
-        if (matrix[row][med] == target)
-            return matrix[row][med];
-        else if (matrix[row][end] <= target) // maximum value out of bound
-            return matrix[row][end];
-        else
-            return matrix[row][start];
-    }
-
-    private static boolean crossSearch(int[][] matrix, int target, int diag_starter) {
-        int m = matrix.length - 1;
-        int n = matrix[0].length - 1;
-        int col_ans = Solution.binarySearch_bycolumns(matrix, diag_starter, target, 0, n);
-        int row_ans = Solution.binarySearch_byrows(matrix, diag_starter, target, 0, m);
-        return (col_ans == target) || (row_ans == target);
-    }
-
     public static boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length - 1;
         int n = matrix[0].length - 1;
-        int min_mn = Math.min(m, n);
-
-        int dig_starter1 = Solution.binarySearch_bydiagonal(matrix, target, 0, min_mn, 0, 0);
-        boolean dig1_ans = Solution.crossSearch(matrix, target, dig_starter1);
-
-        int row_shift = 0;
-        int col_shift = 0;
-        if (m < n) {
-            col_shift = (n - m);
-        } else {
-            row_shift = (m - n);
+        int i = 0;
+        int j = n;
+        while (j >= 0 && i <= m) {
+            if (target > matrix[i][j])
+                i++;
+            else if (target < matrix[i][j])
+                j--;
+            else
+                return true;
         }
-        int dig_starter2 = Solution.binarySearch_bydiagonal(matrix, target, 0, min_mn, row_shift, col_shift);
-        boolean dig2_ans = Solution.crossSearch(matrix, target, dig_starter2 + col_shift + row_shift);
-        return dig1_ans || dig2_ans;
+        return false;
     }
 
     public static void main(String[] args) {
