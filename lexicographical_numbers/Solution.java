ad0442b70df999a147bbb01bd00f0fb17b49c767
Yingjie <1275963@gmail.com>
2016-12-10 00:29:09 -0500

    Wrong understanding of the question

--- lexicographical_numbers/Solution.java
+++ lexicographical_numbers/Solution.java
@@ -3,8 +3,11 @@ package lexicographical_numbers;
 /**
  * Created by y1275963 on 12/9/16.
  */
+import edu.princeton.cs.algs4.StdIn;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
     public static List<Integer> lexicalOrder(int n) {
@@ -15,25 +18,40 @@ public class Solution {
         int p = 0;
         for (int i = 1; i <= 9; i++) {
             int i_temp = i;
-            while (i_temp < n) {
+            while (i_temp <= n) {
                 index_array[p] = i_temp;
                 p++;
                 i_temp = i_temp * 10;
             }
         }
 
-        int[] acc_array = new int[group_length];
-        acc_array[0] = index_array[0];
-        for (int i = 1; i < group_length - 1; i++) {
-            acc_array[i] = acc_array[i - 1] + index_array[i];
+        int[] acc_array = new int[group_length + 1];
+        acc_array[0] = 0;
+        acc_array[1] = 1;
+        for (int i = 0; i < group_length; i++) {
+            int max_range = (int) Math.pow(10, (int) Math.log10(index_array[i]));
+            acc_array[i + 1] = acc_array[i] + (max_range > (n - index_array[i] + 1) ? (n - index_array[i] + 1) : max_range ) ;
         }
         for (int item : index_array)
             System.out.println(item);
         System.out.println();
-        for (int item : acc_array)
-            System.out.println(item);
+//        for (int item : acc_array)
+//            System.out.println(item);
 
-        return null;
+        for (int i = 0; i < index_array.length; i++) {
+            int starter = index_array[i];
+            for (int j = acc_array[i]; j < acc_array[i + 1]; j++) {
+                re.add(starter);
+                starter = starter + 1;
+            }
+        }
+
+//        System.out.println();
+//        for (int item : re) {
+//            System.out.println(item);
+//        }
+
+        return re;
     }
 
     private static int cal_grouplength(int n) {
@@ -43,6 +61,17 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        Solution.lexicalOrder(13);
+        int n = 123;
+        String[] temp = new String[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = Integer.toString(i);
+        }
+        Arrays.sort(temp);
+        for (String item : temp) {
+            System.out.println(item);
+        }
+//        Solution.lexicalOrder(105);
+//        System.out.println(Solution.cal_grouplength(10));
+//        System.out.println(Solution.cal_grouplength(10));
     }
 }
