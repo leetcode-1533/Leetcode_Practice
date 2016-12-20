37ad7b1514309d49defd71ffb73da08b5f3d59ab
Yingjie <1275963@gmail.com>
2016-12-20 15:45:40 -0500

    Passed but slow, may dut to recusion and multiply calls

--- first_bad_version/Solution.java
+++ first_bad_version/Solution.java
@@ -10,7 +10,7 @@ public class Solution extends VersionControl {
 
     private int binarysearch(int start, int end) {
         if (end - 1 > start) {
-            int mid = (start + end) / 2;
+            int mid = start + (end-start) / 2;
             if (!super.isBadVersion(mid - 1) && super.isBadVersion(mid)) {
                 return mid;
             } else if (super.isBadVersion(mid - 1) && super.isBadVersion(mid)) {
@@ -25,10 +25,10 @@ public class Solution extends VersionControl {
     }
 
     public static void main(String[] args) {
-//        int[] inputs = {1,22,34,40,5232,6342342,71231, 2126753390};
-//        int[] targets = {1, 10, 20, 25, 323, 45454, 23232, 1702766719};
-        int[] inputs = {2126753390};
-        int[] targets = {1702766719};
+        int[] inputs = {1,22,34,40,5232,6342342,71231, 2126753390};
+        int[] targets = {1, 10, 20, 25, 323, 45454, 23232, 1702766719};
+//        int[] inputs = {2126753390};
+//        int[] targets = {1702766719};
         Solution Sol = new Solution();
 
         for (int i = 0; i < inputs.length; i++) {
